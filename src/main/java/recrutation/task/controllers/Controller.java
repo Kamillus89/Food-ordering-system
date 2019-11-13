25453292c532b26f7080e4ad9c6590e90f4dbabe
Kamillus89 <kamil.m.olsza@gmail.com>
2019-11-13 18:37:14 +0100

    Add main manu

--- src/main/java/recrutation/task/controllers/Controller.java
+++ src/main/java/recrutation/task/controllers/Controller.java
@@ -8,4 +8,33 @@ public class Controller {
         View view = new View();
         view.getWellcomeMessage();
     }
+
+    View view = new View();
+
+    public Controller() {
+        run();
+    }
+
+    public void run() {
+        showStartMenu();
+    }
+
+    public void showStartMenu() {
+        boolean finish = false;
+        String usersChoice;
+        do {
+            view.showMainMenu();
+            usersChoice = view.getUserChoice().toUpperCase();
+            switch (usersChoice) {
+                case "D":
+                    System.out.println("User choose drinks");
+                    break;
+                case "Q":
+                    finish = true;
+                    break;
+                default:
+                    view.pleaseProvideCorrectOption();
+            }
+        } while (!finish);
+    }
 }
--- src/main/java/recrutation/task/views/View.java
+++ src/main/java/recrutation/task/views/View.java
@@ -1,8 +1,28 @@
 package recrutation.task.views;
 
+import java.util.Scanner;
+
 public class View {
 
     public void getWellcomeMessage() {
-        System.out.println("Wellcome in Food ordering app, how can I help you?");
+        System.out.println("Wellcome in Food ordering app, how can I help you?\n");
+    }
+
+
+    public void showMainMenu() {
+        System.out.println("What would you like to order: " +
+                "\n   [D] Drinks " +
+                "\n   [M] Meals " +
+                "\n   [Q] Quit");
+    }
+
+    public String getUserChoice() {
+        Scanner scan = new Scanner(System.in);
+        String usersChoice = scan.nextLine();
+        return usersChoice;
+    }
+
+    public void pleaseProvideCorrectOption() {
+        System.out.println("Please press rigth key from menu\n");
     }
 }
