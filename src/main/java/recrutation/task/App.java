1027933d41302d7af51bb2e6fb43b082e0a07b53
Kamillus89 <kamil.m.olsza@gmail.com>
2019-11-13 15:25:27 +0100

    .gitignore final update

---
+++ src/main/java/recrutation/task/App.java
@@ -0,0 +1,10 @@
+package recrutation.task;
+
+import recrutation.task.controllers.Controller;
+
+public class App
+{
+    public static void main( String[] args ) {
+        Controller controller = new Controller();
+    }
+}
