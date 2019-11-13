9f9f1d68643dfff189e7f56bf7f931bea41e18bc
Kamillus89 <kamil.m.olsza@gmail.com>
2019-11-13 14:42:29 +0100

    .gitignore update
    .gitignore 2nd update
    
    .gitignore update
    
    .gitignore final update

---
+++ src/main/java/recrutation/task/controllers/Controller.java
@@ -0,0 +1,11 @@
+package recrutation.task.controllers;
+
+import recrutation.task.views.View;
+
+public class Controller {
+
+    static {
+        View view = new View();
+        view.getWellcomeMessage();
+    }
+}
---
+++ src/main/java/recrutation/task/views/View.java
@@ -0,0 +1,8 @@
+package recrutation.task.views;
+
+public class View {
+
+    public void getWellcomeMessage() {
+        System.out.println("Wellcome in Food ordering app, how can I help you?");
+    }
+}
